128c505ade1b0f3c6cc167f3f36ab33c4557c30b
cinber <scberahman@gmail.com>
2021-11-14 13:36:55 +0100

    added comments and description

--- gcode/gcode.py
+++ gcode/gcode.py
@@ -1,17 +1,32 @@
+# from ..ChalkBot import *
 import re
 
 class gcode():
     def readCode(file):
-        # gcode = file.readlines()
+        '''
+        parsing gcode file and adding parameters to a dict
+
+        input: gcode file
+        output: dictionary
+        
+        dictionary keys:
+            gcode,
+            fcode,
+            coordinates
+            
+        '''
         gdict = {}
         i = 0
         for line in file:
             line = line.strip()
             
+            # parsing coordinates, gcode and fcode 
+            # todo: adding curve
             gc = re.findall(r'[G].?\d+', line)
             fc = re.findall(r'[F].?\d+', line)
             coord = re.findall(r'[XY].?\d+.\d+', line)
             
+            # adding data to dict
             if coord or gc or fc:
                 gdict[i] = {'gcode': gc,'fcode': fc, 'coord':coord} 
                 i+=1
@@ -20,8 +35,17 @@ class gcode():
                 # print("{}".format(fc[0]))
                 
         return gdict
+    
+    def printCode(dict):
+        '''
+        input: gcode dictionary
+        output: chalkbot commands
+        '''
+        # if botClient.orientation == 0 and dict[0]['coord'][0]
+        
+        return
 
 if __name__ == "__main__":
     file = open('example.gcode','r').readlines()
     output = gcode.readCode(file)
-    print(output)
+    print(output[0]['coord'][0])
