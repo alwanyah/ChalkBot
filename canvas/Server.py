a02665af98c58657c910901a563105e37f662190
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2021-11-24 13:38:37 +0100

    removed obsolete code added example MyHandler responding to various POST requests

--- canvas/Server.py
+++ canvas/Server.py
@@ -1,12 +1,6 @@
-from __future__ import print_function
-
-# note this for Pythong 2.7 and 3.x compatibility
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler
-  import SocketServer as socketserver
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler
-  import socketserver
+#https://docs.python.org/3/library/http.server.html
+from http.server import SimpleHTTPRequestHandler, BaseHTTPRequestHandler
+import socketserver
 
 
 import threading
@@ -15,12 +9,46 @@ import json
 
 PORT = 8000
 
-# debug stuff
+# debug stuff: print all methods of an object
 def print_methods(o):
   object_methods = [method_name for method_name in dir(o) if callable(getattr(o, method_name))]
   print(object_methods)
 
 module = None
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        print("executing do_POST")
+
+        # get the length of the data to read
+        length = int(self.headers.get('content-length'))
+        data = self.rfile.read(length)
+        print(data)
+
+        print(self.path)
+        
+         
+        if self.path == "/status_motion":
+            response = "stopped"
+        elif self.path == "/status_imu":
+            response = "no imu here"
+        elif self.path == "/orientation":
+            response = "1.73"
+        elif self.path == "/pose":
+            response = "1000;300;1.73"
+        else:
+            response = "unknown command: " + self.path
+        
+        print(response)
+        
+        # header
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
+        # response
+        self.wfile.write(str(response).encode("utf8"))
 
 
 class ChalkbotHandler(SimpleHTTPRequestHandler):
@@ -44,13 +72,14 @@ class ChalkbotHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         try:
           # get the length of the data to read
-          # python 2.7
-          if hasattr(self.headers, 'getheader'):
-            length = int(self.headers.getheader('content-length'))
-          else: # python 3.x
-            length = int(self.headers.get('content-length'))
-
+          length = int(self.headers.get('content-length'))
           data = self.rfile.read(length)
+
+          print(self.requestline)
+          print(self.path)
+          print(self.command)
+
+          print(self.headers.keys())
 
           print(length)
           print(data)
@@ -83,7 +112,8 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 if __name__ == "__main__":
 
   ThreadedTCPServer.allow_reuse_address = True
-  server = ThreadedTCPServer(("", PORT), ChalkbotHandler)
+  #server = ThreadedTCPServer(("", PORT), ChalkbotHandler)
+  server = ThreadedTCPServer(("", PORT), MyHandler)
   
   print("Serving at port: {}".format(PORT))
   server.serve_forever()
