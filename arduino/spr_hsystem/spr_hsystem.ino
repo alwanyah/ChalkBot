c442d7a96875bef1b35ef7959295a9b97bcab0ba
akokay <45182926+akokay@users.noreply.github.com>
2022-01-24 17:26:13 +0100

    Update sprühsystem test

--- arduino/spr_hsystem/spr_hsystem.ino
+++ arduino/spr_hsystem/spr_hsystem.ino
@@ -13,6 +13,8 @@ int enB = 3;
 int in3 = 5;
 int in4 = 4;
 
+int x =0;
+
 
   float shuntvoltage = 0;
   float busvoltage = 0;
@@ -25,7 +27,7 @@ void setup()
   // Set all the motor control pins to outputs
   //pinMode(enA, OUTPUT);
 
-  TCCR2B = TCCR2B & B11111000 | B00000001;
+  //TCCR2B = TCCR2B & B11111000 | B00000001;
   
   pinMode(enB, OUTPUT);
   //pinMode(in1, OUTPUT);
@@ -43,75 +45,47 @@ void setup()
     while (1) { delay(10); }
   }
   Serial.begin(9600);
+  //Serial.println("start");
 }
 
 long t0 = millis();
 int state = 0;
 int lastState = 0;
+int loopamount = 0;
 
 void loop() 
 {
-  long stateTime = millis() - t0;
-  
+ // long stateTime = millis() - t0;
+  /*
   if(state == 0)
   {
-    if(stateTime > 1000) {
-      state = 0;
+    if(loopamount > 3) {
+      state = 1;
+      loopamount=0;
     }
-
-    //
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-    analogWrite(enB, 255);
+    digitalWrite(enB, HIGH);
   } 
   else if(state == 1) 
   {
-    if(stateTime > 1000) {
+    if(loopamount > 7) {
       state = 0;
+      loopamount=0;
     }
-
-    //
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-    analogWrite(enB, 0);
+    digitalWrite(enB, 0);
   }
-
-  if(lastState != state) {
-    t0 = millis();
-  }
-  lastState = state;
-
+  loopamount=loopamount+1;*/
+  digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    digitalWrite(enB, HIGH);
+  //Serial.print(state); 
+  //Serial.print(" ");
   getVoltage();
-  
-  //analogWrite(enA, 0);
-  //analogWrite(enB, 0);
-  //digitalWrite(in1, HIGH);
-  //digitalWrite(in2, LOW);
+  delay(10);
 
-  /*
-  //Serial.println("in3=L, in4=H, enB=255");
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enB, 230);
-  for (int i = 130; i <=255; i++) {
-    //Serial.print(i);Serial.println(" ");
-    analogWrite(enB, i);
-    delay(100);
-  }
-  
-  //delay(300);
-  for (int i = 255; i >= 130; --i) {
-    //Serial.print(i);Serial.println(" ");
-    analogWrite(enB, i);
-    delay(100);
-  }
-  //Serial.print(ina219.getBusVoltage_V()); Serial.println(" ");
-  //getVoltage();
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  analogWrite(enB, 0);
-  delay(100);
-  */
 }
 
 void getVoltage()
@@ -122,12 +96,12 @@ void getVoltage()
   power_mW = ina219.getPower_mW();
   loadvoltage = busvoltage + (shuntvoltage / 1000);
   
-  Serial.print(busvoltage); Serial.println(" ");
-  //Serial.print(shuntvoltage); Serial.println(" ");
-  //Serial.print(loadvoltage); Serial.println(" ");
-  //Serial.print(current_mA); Serial.println(" ");
-  //Serial.print(power_mW); Serial.println(" ");
-  //Serial.println("");
+  Serial.print(busvoltage); 
+  Serial.print(" ");
+  Serial.print(shuntvoltage); Serial.print(" ");
+  Serial.print(loadvoltage); Serial.print(" ");
+  Serial.print(current_mA); Serial.print(" ");
+  Serial.println(power_mW);
   
 }
 
@@ -135,8 +109,8 @@ void getVoltage()
 void directionControl() {
   // Set motors to maximum speed
   // For PWM maximum possible values are 0 to 255
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+  digitalWrite(enA, HIGH);
+  digitalWrite(enB, HIGH);
 
   // Turn on motor A & B
   digitalWrite(in1, HIGH);
@@ -169,15 +143,15 @@ void speedControl() {
   
   // Accelerate from zero to maximum speed
   for (int i = 0; i < 256; i++) {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
+    digitalWrite(enA, i);
+    digitalWrite(enB, i);
     delay(20);
   }
   
   // Decelerate from maximum speed to zero
   for (int i = 255; i >= 0; --i) {
-    analogWrite(enA, i);
-    analogWrite(enB, i);
+    digitalWrite(enA, i);
+    digitalWrite(enB, i);
     delay(20);
   }
   
