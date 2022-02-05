c52f0e23d3abedb6220bf3092c87008cb05a2ddd
afkadrian <85344547+afkadrian@users.noreply.github.com>
2022-02-05 17:17:03 +0100

    Added Offset to get the printng location

--- Simulation/Simulation.py
+++ Simulation/Simulation.py
@@ -14,17 +14,9 @@ robot = Robot.Robot()
 factor = 1
 
 dots = []
-
 radius = 10/factor
+offset = [30/factor, 20/factor]
 
-
-
-# This class represents the shape of the robot, moving on the simulation canvas.
-# The metrics used by Robot.py are based on the real ChalkBot and the coordinates are in mm.
-# For the simulation we choose a default metric of one pixel equals one cm,
-# thus we transform the robot coordinates to the simulation coordinates by dividing with 10
-# as well as dividing by our adjustable 'factor' parameter,
-# which is to determine the scale of the simulation.
 def signal_handler(sig, frame):
     CommandServer.close()
     print("\nClosing Server!")
@@ -32,6 +24,12 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# This class represents the shape of the robot, moving on the simulation canvas.
+# The metrics used by Robot.py are based on the real ChalkBot and the coordinates are in mm.
+# For the simulation we choose a default metric of one pixel equals one cm,
+# thus we transform the robot coordinates to the simulation coordinates by dividing with 10
+# as well as dividing by our adjustable 'factor' parameter,
+# which is to determine the scale of the simulation.
 class ChalkBot(object):
     def __init__(self):
         self.shape = pantograph.Image("tutrobot.png",robot.x/(10*factor), robot.y/(10*factor), 60/factor, 40/factor)
@@ -48,16 +46,6 @@ class ChalkBot(object):
 
         self.shape.draw(canvas)
 
-#def translatePrint(theta):
-#
-#    x = (20*math.cos(theta)/factor) - (30*math.sin(theta)/factor)
-#    y = (20*math.sin(theta)/factor) + (30*math.cos(theta)/factor)
-#
-#    return [x, y]
-
-
-
-
 class ChalkBotSimulation(pantograph.PantographHandler):
     def setup(self):
         self.xvel = 0
@@ -69,9 +57,7 @@ class ChalkBotSimulation(pantograph.PantographHandler):
 
         gc.disable()
         if robot.printing:
-            #offset = translatePrint(robot.theta)
-            #print(offset)
-            dots.append([self.chalkbot.shape.x, self.chalkbot.shape.y])
+            dots.append([self.chalkbot.shape.x + offset[0], self.chalkbot.shape.y + offset[1]])
         for dot in dots:
             pantograph.Circle(dot[0], dot[1], radius, "#f00").draw(self)
         gc.enable()
