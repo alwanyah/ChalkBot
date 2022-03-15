1cdd9ce00581163b42f5f81538ef19a39ada0b85
Sam Berahman <scberahman@gmail.com>
2022-03-15 17:54:32 +0100

    drawing multiple images

--- ChalkBotControl/canvas_drawing.py
+++ ChalkBotControl/canvas_drawing.py
@@ -1,10 +1,11 @@
+from calendar import c
 from ChalkBot import ChalkBotHTTPClient
 import json
 from time import sleep
 
 if __name__ == "__main__":
 
-    var = input("Type 0 to draw with the ChalkBot, type 1 to draw in the Simulation: ")
+    var = input("Type 0 to draw with the ChalkBot, \ntype 1 to draw in the Simulation: ")
     if var == "0":
         chalkbot = ChalkBotHTTPClient("10.0.4.99")
     elif var == "1":
@@ -16,23 +17,24 @@ if __name__ == "__main__":
     file = open("../canvas/coords.json", "r")
     coordinates = json.load(file)
     file.close()
-    
-    # need to scale coordinates to make the drawing more visable
-    for i in range (0, len(coordinates['coords'][0])):
-        coordinates['coords'][0][i][0] *= 10
-        coordinates['coords'][0][i][1] *= 10
+    for j in range(0, len(coordinates['coords'])):
+        # need to scale coordinates to make the drawing more visable
+        for i in range(0, len(coordinates['coords'][j])):
+            coordinates['coords'][j][i][0] *= 10
+            coordinates['coords'][j][i][1] *= 10
 
-    for i in range (0, len(coordinates['coords'][0])):
-        # need to wait until goto is done drawing
-        while (True):
-            if (chalkbot.status() == "stopped"):
-                break;
+    for j in range(0, len(coordinates['coords'])-1):
+        for i in range(0, len(coordinates['coords'][j])):
+            # need to wait until goto is done drawing
+            while (True):
+                if (chalkbot.status() == "stopped"):
+                    break;
 
-        # print("Going to x:", coordinates['coords'][0][i][0], ", y:", coordinates['coords'][0][i][1])
-        # move to first coordinate without drawing, draw the rest
-        if i == 0:
-            chalkbot.goto(coordinates['coords'][0][i][0], coordinates['coords'][0][i][1], 0)
-        else:
-            chalkbot.goto(coordinates['coords'][0][i][0], coordinates['coords'][0][i][1], 255)
-        # sleep for 50 ms because deltaTime in Simulation would be zero and thus divide by 0 and so no gotos are skipped
-        sleep(0.05)
+            # print("Going to x:", coordinates['coords'][0][i][0], ", y:", coordinates['coords'][0][i][1])
+            # move to first coordinate without drawing, draw the rest
+            if i == 0:
+                chalkbot.goto(coordinates['coords'][j][i][0], coordinates['coords'][j][i][1], 0)
+            else:
+                chalkbot.goto(coordinates['coords'][j][i][0], coordinates['coords'][j][i][1], 255)
+            # sleep for 50 ms because deltaTime in Simulation would be zero and thus divide by 0 and so no gotos are skipped
+            sleep(0.05)
