e13fba2cc3fdbdfdfebad10f7ceef45b0c0bd653
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2022-05-09 16:17:36 +0200

    a more correct velocity estimation

--- arduino/encoder_test/EncoderTestSpeed/EncoderTestSpeed.ino
+++ arduino/encoder_test/EncoderTestSpeed/EncoderTestSpeed.ino
@@ -1,4 +1,6 @@
 
+
+
 // ServoCity
 // 5203 Series Yellow Jacket Planetary Gear Motor
 // Ratio: 19.2:1 Ratio
@@ -13,12 +15,21 @@ int encoderPinB = 3;
 //volatile int countA = 0;
 //volatile int countB = 0;
 
-unsigned long last_change_time = 0;
+// high precision speed measurement
+volatile unsigned long last_change_time = 0;
 volatile float velocity = 0;
-
-int dir = 0;  // 1 = CW
+volatile float dir = 0.0f;
+              // 1 = CW
               // 0 = Stationary
               // -1 = CCW
+
+// mean speed measurement
+volatile int count;
+unsigned long last_mean_time = 0;
+float mean_velocity = 0;
+
+// filtered speed
+float filtered_velocity = 0;
 
 //faster than digitalRead()
 #define readA bitRead(PIND,encoderPinA)
@@ -35,14 +46,20 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(encoderPinB), onB, CHANGE);
 
   last_change_time = micros();
+  last_mean_time = micros();
 }
 
 void updateVelocity() 
 {
+  count++;
+  
   unsigned long current_time = micros();
 
   float dt = (current_time - last_change_time);
-  velocity = radPerPulse / dt; // rad/s
+  float v = radPerPulse / dt * dir; // rad/s
+
+  const float alpha = 0.05;
+  velocity = velocity*(1.0f - alpha) + v*alpha;
 
   // update time
   last_change_time = current_time;
@@ -60,7 +77,9 @@ void onA() {
   */
 
   if(readA == readB) {
-    velocity = -velocity;
+    dir = -1.0f;
+  } else {
+    dir = 1.0f;
   }
 }
 
@@ -75,14 +94,32 @@ void onB() {
   }
   */
 
-  if(readA == readB) {
-    velocity = -velocity;
+  if(readA != readB) {
+    dir = -1.0f;
+  } else {
+    dir = 1.0f;
   }
 }
 
 
 // the loop function runs over and over again forever
-void loop() {
+void loop() 
+{
+  // estimate mean velocity
+  unsigned long current_time = micros();
+  if(current_time > last_mean_time + 1000000/10) { // 1s
+    float dt = (current_time - last_mean_time);
+    mean_velocity = ((float)count) * radPerPulse * dir / dt;
+
+    if(count == 0) {
+      velocity = 0;
+    }
+    
+    count = 0;
+    last_mean_time = current_time;
+  }
+
+  filtered_velocity = 0.9f*mean_velocity + 0.1*velocity;
 
   // uncomment to see squarewave output
   /*
@@ -95,9 +132,17 @@ void loop() {
   
   Serial.print(velocity); // verlocity in rad/s
   Serial.print(",");
-  Serial.print(velocity*180.0f/PI); // verlocity in deg/s
-  Serial.print(",");
+  //Serial.print(velocity*180.0f/PI); // verlocity in deg/s
+  //Serial.print(",");
   Serial.print(velocity / (2.0f*PI) * 60.0); // RPM
+  Serial.print(",");
+  
+  Serial.print(filtered_velocity); // verlocity in rad/s
+  Serial.print(",");
+  //Serial.print(filtered_velocity*180.0f/PI); // verlocity in deg/s
+  //Serial.print(",");
+  Serial.print(filtered_velocity / (2.0f*PI) * 60.0); // RPM
+  
   Serial.println();  
   
 }
